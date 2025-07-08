fde5793b2a1d6fc5e67d36d64f06573b23413a8c
Shraddha <shraddhashaileshgawade@gmail.com>
2025-07-09 00:46:02 +0100

    Check done some changes in the old files and added new code of randomforest model

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
-TYPE2 = 'Type2'
-TYPE3 = 'Type3'
-TYPE4 = 'Type4'
-INTERACTION_CONTENT = 'interaction_content'
+class Config:
+    # Input Columns
+    TICKET_SUMMARY = 'Ticket Summary'
+    INTERACTION_CONTENT = 'Interaction content'
+
+    # Target/Type columns to test or predict
+    TYPE2 = 'y2'
+    TYPE3 = 'y3'
+    TYPE4 = 'y4'
+
+    TYPE_COLS = [y2, y3, y4]
+
+    # If you have a main class or grouped label
+    CLASS_COL = y2  # or whichever you want as main class
